cadd9adfac9b4c683ce0a5e614d7f7d8e0168618
ssunhong <bebo007@naver.com>
2025-05-03 10:44:48 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Pagination from "react-bootstrap/Pagination";
+import "./App.css";
 
 function App() {
+  let active = 2;
+  let items = [];
+  for (let number = 1; number <= 5; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === active}>
+        {number}
+      </Pagination.Item>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row xs={1} md={4} className="g-4 mb-4">
+        {Array.from({ length: 16 }).map((_, idx) => (
+          <Col key={idx}>
+            <Card className="shadow-sm hover-shadow">
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs="auto">
+          <Pagination>{items}</Pagination>
+        </Col>
+      </Row>
+    </>
   );
 }
 
